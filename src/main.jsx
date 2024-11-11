e58c0cbbcaa216396efc82d5f503aca9f0dc77d7
Mona Ueno <monaueno@Monas-MacBook-Air.local>
2024-11-10 19:53:54 -0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Import global reset or basic styles
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
