a4f387d5de243ceaf25cae3390d49c351da1402a
Mona Ueno <monaueno@Monas-MacBook-Air.local>
2024-11-10 19:52:04 -0700

    fixed main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // Import global reset or basic styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
