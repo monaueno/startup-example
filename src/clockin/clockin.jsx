5e550a0c6c762898398428ea19dc3bb80e834993
Mona Ueno <monaueno@Monas-MacBook-Air.local>
2024-11-17 17:56:08 -0700

    css for ClockIn added

--- src/clockin/clockin.jsx
+++ src/clockin/clockin.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Workers } from './ClockIn/workers.jsx';
 import { TimeClock } from './ClockIn/timeclock.jsx'
 import { LogTimeProvider } from './ClockIn/LogTime.jsx';
+import './ClockIn.css'
 
 export function ClockIn(props) {
     return (
