8ff325f70bfebb427c3929bf3c56925282f8ae18
Mona Ueno <monaueno@Monas-Air.app.byu.edu>
2024-11-20 11:01:12 -0700

    ./

---
+++ src/app.jsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import MyPay from './mypay/MyPay';
+import ClockIn from './clockin/ClockIn';
+import Profile from './profile/profile';
+import Login from './logIn/logIn';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const isAuthenticated = true;
+  console.log(isAuthenticated)
+
+  return (
+    <BrowserRouter>
+      <div className='body bg-light text-dark'>
+        {/* Header */}
+        <header className='container-fluid'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-primary'>
+            <div className='container-fluid'>
+              <NavLink className='navbar-brand text-light' to='/'>
+                TodayPay!
+              </NavLink>
+              <div className='collapse navbar-collapse'>
+                <ul className='navbar-nav me-auto'>
+                  {!isAuthenticated && (
+                    <li className='nav-item'>
+                      <NavLink className='nav-link text-light' to='/'>
+                        Log In
+                      </NavLink>
+                    </li>
+                  )}
+                  {isAuthenticated && (
+                    <>
+                      <li className='nav-item'>
+                        <NavLink className='nav-link text-light' to='/clockin'>
+                          Clock In
+                        </NavLink>
+                      </li>
+                      <li className='nav-item'>
+                        <NavLink className='nav-link text-light' to='/mypay'>
+                          My Pay
+                        </NavLink>
+                      </li>
+                      <li className='nav-item'>
+                        <NavLink className='nav-link text-light' to='/profile'>
+                          Profile
+                        </NavLink>
+                      </li>
+                    </>
+                  )}
+                </ul>
+                {isAuthenticated && (
+                  <button
+                    className='btn btn-danger'
+                    onClick={() => {
+                      setUserName('');
+                      localStorage.removeItem('userName');
+                    }}
+                  >
+                    Log Out
+                  </button>
+                )}
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        {/* Routes */}
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                onAuth={(userName) => {
+                  setUserName(userName);
+                  localStorage.setItem('userName', userName);
+                }}
+              />
+            }
+          />
+          {isAuthenticated && <Route path='/clockin' element={<ClockIn userName={userName} />} />}
+          {isAuthenticated && <Route path='/mypay' element={<MyPay />} />}
+          {isAuthenticated && <Route path='/profile' element={<Profile />} />}
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        {/* Footer */}
+        <footer className='bg-dark text-light py-3'>
+          <div className='container-fluid text-center'>
+            <span className='text-reset'>Mona Ueno</span>
+            <br />
+            <a
+              className='text-reset'
+              href='https://github.com/monaueno/startup-example/blob/main/README.md'
+            >
+              Github
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <main className='container-fluid bg-secondary text-center py-5'>
+      <h1>404: Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </main>
+  );
+}
