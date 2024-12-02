4248980a4982971a41f193d9670853647ea80968
Mona Ueno <monaueno@Monas-Air.app.byu.edu>
2024-12-02 13:44:36 -0700

    login.css imported

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,14 +3,24 @@ import React from 'react';
 import { Unauthenticated } from './unauthenticated';
 import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
+import './login.css'
 
 export default function Login({ userName, authState, onAuthChange }) {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>
-        {authState !== AuthState.Unknown && <h1>Welcome to TodayPay!</h1>}
+    <main className="login-page">
+      {/* Left Section */}
+      <div className="login-left">
+        <h1>Get Paid Today!</h1>
+      </div>
+
+      {/* Right Section */}
+      <div className="login-right">
+        {authState !== AuthState.Unknown && <h1>Please Login</h1>}
         {authState === AuthState.Authenticated && (
-          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+          <Authenticated
+            userName={userName}
+            onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)}
+          />
         )}
         {authState === AuthState.Unauthenticated && (
           <Unauthenticated
