f93208edd56fd5b40cc558d5db9410fb360d0925
Mona Ueno <monaueno@Monas-MacBook-Air.local>
2024-12-09 20:42:27 -0700

    save

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '/src/app.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log("Hello")
+root.render(<App />);
